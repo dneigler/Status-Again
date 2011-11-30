76f20434f20d278ae50a98874ef77ddb485af46f
unknown <davidne@DAVIDNE-PC6.saccap.int>
2011-11-30 18:41:32 -0500

    Tweaking NLog

--- Status.BLL/StatusReportManager.cs
+++ Status.BLL/StatusReportManager.cs
@@ -1,14 +1,15 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
-using NLog;
 using Status.Etl;
 using Status.Model;
+using NLog;
+
 
 namespace Status.BLL
 {
     public class StatusReportManager : IStatusReportManager
     {
-        private readonly Logger _logger;
+        private readonly Logger _logger = NLog.LogManager.GetCurrentClassLogger();
 
         public IRollStatusProcessor RollStatusProcessor { get; set; }
 
@@ -16,9 +17,8 @@ namespace Status.BLL
 
         public IStatusEtl StatusEtl { get; set; }
 
-        public StatusReportManager(Logger logger, IRollStatusProcessor rollStatusProcessor, IRollStatusDateProcessor rollStatusDateProcessor, IStatusEtl statusEtl)
+        public StatusReportManager(IRollStatusProcessor rollStatusProcessor, IRollStatusDateProcessor rollStatusDateProcessor, IStatusEtl statusEtl)
         {
-            _logger = logger;
             RollStatusProcessor = rollStatusProcessor;
             RollStatusDateProcessor = rollStatusDateProcessor;
             StatusEtl = statusEtl;
@@ -30,6 +30,7 @@ namespace Status.BLL
         /// <param name="report"></param>
         public StatusReport RollStatusReport(StatusReport report)
         {
+            _logger.Debug("Rolling status report from {0:yyyy-mm-dd} started", report.PeriodStart);
             var rolledReport = new StatusReport
                                    {
                                        Caption = report.Caption,
@@ -41,6 +42,7 @@ namespace Status.BLL
                         StatusItem mappedItem = RollStatusProcessor.MapStatusItem(si, rolledReport.PeriodStart);
                         if (mappedItem != null) rolledReport.Items.Add(mappedItem);
                     });
+            _logger.Info("Rolled status report from {0:yyyy-mm-dd} to {1:yyyy-mm-dd}", report.PeriodStart, rolledReport.PeriodStart);
             return rolledReport;
         }
     }
