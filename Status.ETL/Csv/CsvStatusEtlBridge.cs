fe9dbec8911e3238474d4a43f1be086fadcbd1e1
Dave Neigler <dneigler@gmail.com>
2011-12-02 18:40:50 -0500

    Added shell of Bridge class and NLog reference.

---
+++ Status.ETL/Csv/CsvStatusEtlBridge.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NLog;
+
+namespace Status.ETL.Csv
+{
+    public class CsvStatusEtlBridge : ICsvStatusEtlBridge
+    {
+        private readonly Logger _logger = LogManager.GetCurrentClassLogger();
+        
+        public void UpsertStatus(IList<Etl.Csv.StatusCsvItem> items)
+        {
+            _logger.Info("UpsertStatus called for {0} items", items.Count);
+            // things to check
+            // all projects exist and project id's returned
+            var grpProjects = items.GroupBy(item => item.Project).OrderBy(i => i.ToString());
+
+            foreach (var project in grpProjects)
+            {
+                _logger.Debug("Project found: {0}", project);
+            }
+            // items.ToList().ForEach(item => item.
+        }
+    }
+}
---
+++ Status.ETL/Csv/ICsvStatusEtlBridge.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Status.Etl.Csv;
+
+namespace Status.ETL.Csv
+{
+    public interface ICsvStatusEtlBridge
+    {
+        void UpsertStatus(IList<StatusCsvItem> items);
+    }
+}
