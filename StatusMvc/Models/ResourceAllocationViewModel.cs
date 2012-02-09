518d5fe06c2450569a4ce0709fc249cea41443cd
dneigler <dneigler@gmail.com>
2012-02-08 21:58:34 -0500

    ResourceAllocationViewModel stub.

---
+++ StatusMvc/Models/ResourceAllocationViewModel.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace StatusMvc.Models
+{
+    public class ResourceAllocationViewModel
+    {
+        public class TeamAllocationViewModel
+        {
+            public TeamAllocationViewModel()
+            {
+                this.Users = new List<UserAllocationViewModel>();
+            }
+
+            public int TeamId { get; set; }
+
+            public string TeamName { get; set; }
+
+            public IList<UserAllocationViewModel> Users { get; set; }
+
+            /// <summary>
+            /// Class shows allocations by user, project, month w/ allocations.
+            /// </summary>
+            public class UserAllocationViewModel
+            {
+                public UserAllocationViewModel()
+                {
+                    this.Projects = new List<ProjectViewModel>();
+
+                }
+
+                public int ResourceId { get; set; }
+
+                public string ResourceFullName { get; set; }
+
+                public IList<ProjectViewModel> Projects { get; set; }
+            }
+            public class ProjectViewModel
+            {
+                public ProjectViewModel()
+                {
+                    this.MonthlyAllocations = new List<MonthAllocationViewModel>();
+
+                }
+
+                public int ProjectId { get; set; }
+
+                public string ProjectName { get; set; }
+
+                public IList<MonthAllocationViewModel> MonthlyAllocations { get; set; }
+            }
+
+            public class MonthAllocationViewModel
+            {
+                public DateTime Month { get; set; }
+
+                public int ResourceAllocationId { get; set; }
+
+                public decimal Allocation { get; set; }
+
+            }
+        }
+    }
+}
